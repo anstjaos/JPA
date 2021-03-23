6b79cfa40587736a715e442df5282600a23c0702
moonseo.kim <moonseo.kim@nhn.com>
2021-03-23 16:59:38 +0900

    test read result

--- querydsl /src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl /src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
@@ -87,5 +90,20 @@ public class QuerydslBasicTest {
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+//        List<Member> fetch = queryFactory.selectFrom(member).fetch();
+//
+//        Member fetchOne = queryFactory.selectFrom(member).fetchOne();
+//
+//        Member fetchFirst = queryFactory.selectFrom(member).fetchFirst();
+
+        // query가 두번 나간다.
+        QueryResults<Member> results = queryFactory.selectFrom(member).fetchResults();
+
+        long total = results.getTotal();
+        List<Member> content = results.getResults();
     }
 }
